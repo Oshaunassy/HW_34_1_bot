0b5cb8f8119df7e2632b4d9f058568655b0a681e
oshaunassy <burul_nbc@mail.ru>
2023-10-12 19:09:04 +0600

    hw1

--- handlers/callback.py
+++ handlers/callback.py
@@ -1,5 +1,6 @@
 from aiogram import types, Dispatcher
 from config import bot
+from keyboards.inline_buttons import questionnaire_one_keyboard
 
 async def start_questionnaire(call: types.CallbackQuery):
     print(call)
@@ -25,7 +26,7 @@ async def no_answer(call: types.CallbackQuery):
     )
 
 def register_callback_handlers(dp: Dispatcher):
-    dp.register_callback_query_handler(start_questionnaire, lambda call: call.data == "start_questioannaire")
+    dp.register_callback_query_handler(start_questionnaire, lambda call: call.data == "start_questionnaire")
     dp.register_callback_query_handler(yes_answer, lambda call: call.data == "hungry_yes")
     dp.register_callback_query_handler(no_answer, lambda call: call.data == "hungry_no")
 
--- handlers/start.py
+++ handlers/start.py
@@ -21,7 +21,7 @@ async def start_button(message: types.Message):
     await bot.send_message(
         chat_id=message.chat.id,
         text=f"Hello, I'm your bot",
-        reply_markup=await  start_keyboard()
+        reply_markup=await start_keyboard()
     )
 
 def register_start_handlers(dp: Dispatcher):
