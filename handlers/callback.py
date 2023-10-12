1dce9577cf4b505a61b2088af8ba1be2496fc748
oshaunassy <burul_nbc@mail.ru>
2023-10-12 19:01:19 +0600

    first commit

---
+++ handlers/callback.py
@@ -0,0 +1,31 @@
+from aiogram import types, Dispatcher
+from config import bot
+
+async def start_questionnaire(call: types.CallbackQuery):
+    print(call)
+    await bot.send_message(
+        chat_id=call.message.chat.id,
+        text="R u hungry? ",
+        reply_markup=await questionnaire_one_keyboard()
+    )
+
+
+async def yes_answer(call: types.CallbackQuery):
+    print(call)
+    await bot.send_message(
+        chat_id=call.message.chat.id,
+        text="OK"
+    )
+
+async def no_answer(call: types.CallbackQuery):
+    print(call)
+    await bot.send_message(
+        chat_id=call.message.chat.id,
+        text="Glad Y r not hungry",
+    )
+
+def register_callback_handlers(dp: Dispatcher):
+    dp.register_callback_query_handler(start_questionnaire, lambda call: call.data == "start_questioannaire")
+    dp.register_callback_query_handler(yes_answer, lambda call: call.data == "hungry_yes")
+    dp.register_callback_query_handler(no_answer, lambda call: call.data == "hungry_no")
+
